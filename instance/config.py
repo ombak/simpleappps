f87e67fa9811249c5b19091507fc09be36105f4d
Sukma Wijaya Saputra <coklatpanas83@gmail.com>
2019-11-11 19:31:44 +0700

    add .env and make variable DATABASE_URL for heroku database

--- instance/config.py
+++ instance/config.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-SECRET_KEY='1234567890qwertyuiop'
-SQLALCHEMY_DATABASE_URI='postgresql://postgres:pass@word1@localhost/simpleappps_db'
+import os
+
+SECRET_KEY=os.environ.get('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
 SQLALCHEMY_ECHO=True
-SQLALCHEMY_TRACK_MODIFICATIONS=True
+SQLALCHEMY_TRACK_MODIFICATIONS=False
